dd43aef09ed25a0a462d14b2d18ae8df98e71a1c
Mohammed Mansur <95575195+mohammedmansur@users.noreply.github.com>
2022-11-17 22:32:52 +0300

    splash done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
-  runApp(const SplashScreen());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
 }
 
 class SplashScreen extends StatefulWidget {
@@ -27,7 +40,37 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60),
+            height: MediaQuery.of(context).size.height / 2,
+            width: double.infinity,
+            child: Lottie.asset('assets/Lottie/splash.json', fit: BoxFit.cover),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 70),
+            child: Center(
+                child: Text(
+              'Fruity',
+              style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green),
+            )),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'version 1.0.0',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
