2694717557585e36a1149c3c60c5fdb7b90763a2
Mohammed Mansur <95575195+mohammedmansur@users.noreply.github.com>
2022-11-22 23:29:01 +0300

    all tab bar done

--- lib/src/item/Fruit.dart
+++ lib/src/item/Fruit.dart
@@ -1,12 +1,52 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+import 'card.dart';
 
 class Fruit extends StatelessWidget {
   const Fruit({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Color.fromARGB(255, 245, 244, 244),
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          gridItemCard(
+              context: context,
+              name: 'Orange',
+              category: 'Fruits',
+              deatil:
+                  'An orange is a fruit of various citrus species in the family Rutaceae ); it primarily refers to Citrus × sinensis, which is also called sweet orange.',
+              imgPath: 'assets/fruits/orange.png',
+              backImg: Colors.orange[50]!,
+              price: 10.5),
+          gridItemCard(
+              context: context,
+              name: 'Apple',
+              category: 'Fruits',
+              deatil: 'hhhhhhhh',
+              imgPath: 'assets/fruits/apple.png',
+              backImg: Colors.green[50]!,
+              price: 10.5),
+          gridItemCard(
+              context: context,
+              name: 'PinApple',
+              category: 'Fruits',
+              deatil: 'hhhhhhhh',
+              imgPath: 'assets/fruits/pinapple.png',
+              backImg: Colors.yellow[50]!,
+              price: 10.5),
+          gridItemCard(
+              context: context,
+              name: 'Strawberry',
+              category: 'Fruits',
+              deatil: 'hhhhhhhh',
+              imgPath: 'assets/fruits/strawberry.png',
+              backImg: Colors.red[50]!,
+              price: 10.5),
+        ],
+      ),
+    );
   }
 }
--- lib/src/item/card.dart
+++ lib/src/item/card.dart
@@ -111,89 +111,90 @@ Widget gridItemCard(
     required Color backImg,
     required double price}) {
   return GestureDetector(
-      onTap: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => FruitPro(
-                  backImg: backImg,
-                  name: name,
-                  category: category,
-                  deatil: deatil,
-                  imgPath: imgPath,
-                  price: price),
-            ));
-      },
-      child: Container(
-        color: Color.fromARGB(255, 245, 244, 244),
-        child: Container(
-          margin: EdgeInsets.all(10),
-          height: 150,
-          width: 140,
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(30)),
-          child: Stack(
-            children: [
-              Positioned(
-                left: 10,
-                right: 10,
-                child: Image.asset(
-                  imgPath,
-                  height: 110,
-                  width: 110,
+    onTap: () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => FruitPro(
+                backImg: backImg,
+                name: name,
+                category: category,
+                deatil: deatil,
+                imgPath: imgPath,
+                price: price),
+          ));
+    },
+    child: Container(
+      margin: EdgeInsets.all(10),
+      height: 200,
+      width: 160,
+      child: Stack(children: [
+        Column(
+          children: [
+            Center(
+              child: Container(
+                height: 157,
+                width: 160,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
                 ),
-              ),
-              Positioned(
-                right: 0,
-                top: 125,
-                child: GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    height: 40,
-                    width: 40,
-                    decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10),
-                            topLeft: Radius.circular(10))),
-                    child: Center(
-                      child: Icon(
-                        Icons.shopping_bag,
-                        color: Colors.white,
+                child: Stack(children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        imgPath,
+                        height: 100,
+                        width: 100,
                       ),
-                    ),
+                    ],
                   ),
-                ),
+                  Positioned(
+                    left: 20,
+                    right: 20,
+                    bottom: 25,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          name,
+                          style: TextStyle(
+                              fontSize: 17, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          '\$$price',
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  )
+                ]),
               ),
-              Positioned(
-                top: 150,
-                left: 10,
-                child: Text(
-                  name,
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Positioned(
-                top: 180,
-                left: 10,
-                child: Text(
-                  category,
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ),
-              Positioned(
-                top: 200,
-                left: 10,
-                child: Text(
-                  '\$$price',
-                  style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ));
+        Positioned(
+          left: 70,
+          bottom: 0,
+          child: Container(
+            height: 40,
+            width: 40,
+            child: Icon(
+              Icons.add_shopping_cart,
+              color: Colors.white,
+            ),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12), color: Colors.orange),
+          ),
+        )
+      ]),
+    ),
+  );
 }
